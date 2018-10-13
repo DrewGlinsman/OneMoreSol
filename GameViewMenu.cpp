a332712441d44133977ff471b561f39046a963dc
pandacorporal <31940539+pandacorporal@users.noreply.github.com>
2018-10-13 15:16:15 -0400

    Menu Selection works will transition to game screen or quit based on what is selected.
    Just need graphical feedback for what box is selected.

--- GameViewMenu.cpp
+++ GameViewMenu.cpp
@@ -1,5 +1,3 @@
-// Chris Wolinski SFML Pong
-
 //This is where the player views what happens onscreen.
 
 #include "GameViewMenu.h"
@@ -9,99 +7,97 @@ using namespace std;
      GameViewMenu::GameViewMenu() // Menu window constructor
      : menuWindow(sf::VideoMode(1440, 900, 32), "One More Soul",sf::Style::Titlebar | sf::Style::Close)
      {
-        if(!menuFont.loadFromFile("assets/Game_Font.ttf"))       // Constructor loads and sets all items on screen
-            cout << "Could not load requested font." << endl;
-
         if (!menuImage.loadFromFile("assets/Menu_Screen.png"))
            cout << "Could not load requested image." << endl;
 
         if (!menuSound.loadFromFile("assets/Menu_Music.ogg"))
             cout << "Could not load request music." << endl;
 
+        //selector.setPosition(0,0);
+        //cout << "0,0" << endl;
+        background.setSize(sf::Vector2f(1,1));
+
         background.setPosition(0,0);
         background.setSize(sf::Vector2f(1440,900));
         background.setTexture(&menuImage);
 
-        menuTitle.setFont(menuFont);
-        menuTitle.setCharacterSize(75);
-        menuTitle.setColor(sf::Color::Black);
-        menuTitle.setPosition(titleX,titleY);
-        menuTitle.setString("Pong");
 
-        menuPlay1.setFont(menuFont);
-        menuPlay1.setCharacterSize(25);
-        menuPlay1.setColor(sf::Color::White);
-        menuPlay1.setPosition(playX1,playY1);
-        menuPlay1.setString("Press 1 To PLAY with 1 Player");
-
-        menuPlay2.setFont(menuFont);
-        menuPlay2.setCharacterSize(25);
-        menuPlay2.setColor(sf::Color::White);
-        menuPlay2.setPosition(playX2,playY2);
-        menuPlay2.setString("Press 2 To PLAY with 2 Players");
-
-        menuPlay3.setFont(menuFont);
-        menuPlay3.setCharacterSize(25);
-        menuPlay3.setColor(sf::Color::White);
-        menuPlay3.setPosition(playX3,playY3);
-        menuPlay3.setString("Controls P1:(W/S) P2:(Up/Down)");
-
-        menuPlay4.setFont(menuFont);
-        menuPlay4.setCharacterSize(25);
-        menuPlay4.setColor(sf::Color::Cyan);
-        menuPlay4.setPosition(playX4,playY4);
-        menuPlay4.setString("(W/S)");
-
-        menuPlay5.setFont(menuFont);
-        menuPlay5.setCharacterSize(25);
-        menuPlay5.setColor(sf::Color::Magenta);
-        menuPlay5.setPosition(playX5,playY5);
-        menuPlay5.setString("(Up/Down)");
-
-    }
-
-    bool GameViewMenu::menuSelection()
-    {
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Num1)) // End menu loop and set player count
-        {
-            menuWindow.close();
-            return false;
-        }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Num2))
-        {
-            menuWindow.close();
-            return true;
-        }
-    }
-
-    void GameViewMenu::updateMenu(void) // Updates screen
-    {
         menuMusic.setBuffer(menuSound);
 
         menuMusic.play();
 
         menuMusic.setLoop(true);
 
+
+    }
+
+    /*
+    bool GameViewMenu::menuSelection()
+    {
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && !buttonHeld) // End menu loop and set player count
+        {
+            if(sf::Vector2f (0,0) == selector.getPosition())
+            {
+                selector.setPosition(0,2);
+                cout << "0,2" << endl;
+            }
+            else if(sf::Vector2f (0,1) == selector.getPosition())
+            {
+                selector.setPosition(0,0);
+                cout << "0,0" << endl;
+            }
+            else if(sf::Vector2f (0,2) == selector.getPosition())
+            {
+                selector.setPosition(0,1);
+                cout << "0,1" << endl;
+            }
+
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && !buttonHeld)
+        {
+            if(sf::Vector2f (0,0) == selector.getPosition())
+            {
+                selector.setPosition(0,1);
+                cout << "0,1" << endl;
+            }
+            else if(sf::Vector2f (0,1) == selector.getPosition())
+            {
+                selector.setPosition(0,2);
+                cout << "0,2" << endl;
+            }
+            else if(sf::Vector2f (0,2) == selector.getPosition())
+            {
+                selector.setPosition(0,0);
+                cout << "0,0" << endl;
+            }
+
+        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+        {
+            if(sf::Vector2f (0,0) == selector.getPosition())
+            {
+
+            }
+            if(sf::Vector2f (0,1) == selector.getPosition())
+            {
+
+            }
+            if(sf::Vector2f (0,2) == selector.getPosition())
+            {
+
+            }
+        }
+    }
+
+    */
+
+    void GameViewMenu::updateMenu(void) // Updates screen
+    {
+
         menuWindow.clear(sf::Color::Black);
 
         menuWindow.draw(background);
 
-        menuWindow.draw(menuTitle);
-
-        menuWindow.draw(menuPlay1);
-
-        menuWindow.draw(menuPlay2);
-
-        menuWindow.draw(menuPlay3);
-
-        menuWindow.draw(menuPlay4);
-
-        menuWindow.draw(menuPlay5);
-
         // display
         menuWindow.display();
    }
-
-
-
-
--- main.cpp
+++ main.cpp
@@ -14,13 +14,15 @@ int main(int argc, char** argv)
     bool gameOver = false; // Never gets set to true
     bool twoPlayerGame = false;
     bool quit = false;
-
+    sf::Event event;
     while(!gameOver) // Start game loop
     {
         GameViewMenu* currentMenu = new GameViewMenu(); //Create menu window for player
 
         currentMenu -> menuWindow.display();
-
+        sf::RectangleShape selector;
+        selector.setPosition(0,0);
+        cout << "0,0" << endl;
         currentMenu -> updateMenu();
 
         while(currentMenu -> menuWindow.isOpen()) // Menu loop
@@ -31,6 +33,64 @@ int main(int argc, char** argv)
                 {
                     currentMenu -> menuWindow.close(); // Quit game
                     return 0;
+                }
+
+                if(Event.type == sf::Event::KeyPressed)
+                {
+                    if(Event.key.code == sf::Keyboard::Up)
+                    {
+                        if(sf::Vector2f (0,0) == selector.getPosition())
+                        {
+                            selector.setPosition(0,2);
+                            cout << "0,2" << endl;
+                        }
+                        else if(sf::Vector2f (0,1) == selector.getPosition())
+                        {
+                            selector.setPosition(0,0);
+                            cout << "0,0" << endl;
+                        }
+                        else if(sf::Vector2f (0,2) == selector.getPosition())
+                        {
+                            selector.setPosition(0,1);
+                            cout << "0,1" << endl;
+                        }
+                    }
+
+                    if(Event.key.code == sf::Keyboard::Down)
+                    {
+                        if(sf::Vector2f (0,0) == selector.getPosition())
+                            {
+                                selector.setPosition(0,1);
+                                cout << "0,1" << endl;
+                            }
+                            else if(sf::Vector2f (0,1) == selector.getPosition())
+                            {
+                                selector.setPosition(0,2);
+                                cout << "0,2" << endl;
+                            }
+                            else if(sf::Vector2f (0,2) == selector.getPosition())
+                            {
+                                selector.setPosition(0,0);
+                                cout << "0,0" << endl;
+                            }
+                    }
+
+                    if(Event.key.code == sf::Keyboard::Space)
+                    {
+                        if(sf::Vector2f (0,0) == selector.getPosition())
+                        {
+                            currentMenu -> menuWindow.close();
+                        }
+                        if(sf::Vector2f (0,1) == selector.getPosition())
+                        {
+                            currentMenu -> menuWindow.close();
+                        }
+                        if(sf::Vector2f (0,2) == selector.getPosition())
+                        {
+                            currentMenu -> menuWindow.close();
+                            return 0;
+                        }
+                    }
                 }
             }
 
