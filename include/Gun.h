cd113abf19efe7bc67de05d8be2cab155a0becd5
Jack Bowden <jabowden@email.wm.edu>
2018-10-14 22:39:18 -0400

    Think I've fixed gun on Character's end

--- include/Gun.h
+++ include/Gun.h
@@ -7,22 +7,14 @@ class Gun
 public:
     Gun();
     virtual ~Gun();
-<<<<<<< HEAD
 
-=======
->>>>>>> refs/heads/master
     unsigned int damage;
     unsigned int clip;
     float reloadSpeed;
-<<<<<<< HEAD
     std::string dmgMultiplier;
     float fireRate;
     sf::Image image;
 
-=======
-    float dmgMultiplier;
-    float fireRate;
->>>>>>> refs/heads/master
 };
 
 #endif // GUN_H
