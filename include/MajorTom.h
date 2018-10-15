25506c0349a7d053bf83555dfe98300300290f00
SoloCW <ChrisWolinski7@gmail.com>
2018-10-14 23:52:34 -0400

    Major Tom Class Pt.1
    Added CPP and H for Major Tom class. Will add more details later.

---
+++ include/MajorTom.h
@@ -0,0 +1,47 @@
+
+#ifndef MAJORTOM_H
+#define MAJORTOM_H
+
+#include <SFML/Graphics.hpp>
+#include "Gun.h"
+
+using namespace std;
+
+class MajorTom {
+
+    private:
+        float positionX;
+        float positionY;
+        float newPosition;
+        float timePassed;
+        int currentHealth;
+        int newHealth;
+        Gun currentGun;
+        Gun newGun;
+
+    public:
+        sf::RectangleShape majorTom;
+
+        MajorTom ();
+
+        void drawTom(sf::RenderWindow& window);
+
+        float getTomPosition();
+
+        void setTomPosition(float newPosition);
+
+        void moveTomUp(float timePassed);
+
+        void moveTomDown(float timePassed);
+
+        int getHealth();
+
+        void setHealth(int newHealth);
+
+        Gun getGun();
+
+        void setGun(Gun newGun);
+
+};
+
+#endif
