97a8a79192e380e048a4d322485878681fdf7860
Drew Glinsman <31940539+DrewGlinsman@users.noreply.github.com>
2018-10-16 14:56:04 -0400

    added some framework for gamelogic.h

--- include/GameLogic.h
+++ include/GameLogic.h
@@ -2,6 +2,25 @@
 #define GAMELOGIC_H
 
 #include "GameViewPlayer.h"
+#include "MajorTom.h"
+#include "KoratEmpire.h"
 
+using namespace std;
+class GameLogic{
+
+    private:
+        MajorTom majorTom;
+        //wrap it in reference wrapper so that it passes references instead of copies
+        std::list<std::reference_wrapper<KoratEmpire>> currentKorat;
+        sf::SoundBuffer gunSound;
+        sf::Sound gunSoundEffect;
+
+    public:
+        GameLogic();
+        moveKorat();
+        spawnKorat();
+        checkEnd(GameViewPlayer* currentGame);
+
+};
 
 #endif
