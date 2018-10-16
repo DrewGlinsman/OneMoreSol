849a3f054adcdbd0ac2900d2779caf07ac9b50d5
Jack Bowden <jabowden@email.wm.edu>
2018-10-16 14:59:46 -0400

    Grunt furthered.

--- include/Grunt.h
+++ include/Grunt.h
@@ -8,14 +8,20 @@
 #ifndef INCLUDE_GRUNT_H_
 #define INCLUDE_GRUNT_H_
 
+#include <SFML/Graphics.hpp>
 #include "KoratEmpire.h"
 
 class Grunt: public KoratEmpire {
 public:
+	sf::RectangleShape grunt;
+
 	Grunt();
 	virtual ~Grunt();
 
 	void wasShot();
+
+	void drawGrunt(sf::RenderWindow& window);
+
 };
 
 #endif /* INCLUDE_GRUNT_H_ */
