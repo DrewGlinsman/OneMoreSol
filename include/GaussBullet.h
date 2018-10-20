29232e24134f118a649af3572030bff186c5761d
Drew Glinsman <31940539+DrewGlinsman@users.noreply.github.com>
2018-10-20 12:21:33 -0400

    Fixed gauss spelling

---
+++ include/GaussBullet.h
@@ -0,0 +1,17 @@
+/*
+ * GuassBullet.h
+ *
+ *  Created on: Oct 19, 2018
+ *      Author: Bowden
+ */
+
+#ifndef INCLUDE_GAUSBULLET_H_
+#define INCLUDE_GAUSSBULLET_H_
+
+class GaussBullet {
+public:
+	GaussBullet();
+	virtual ~GaussBullet();
+};
+
+#endif /* INCLUDE_GUASSBULLET_H_ */
