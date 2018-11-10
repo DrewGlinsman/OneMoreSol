73a40070602989e787f9384bbee75cb77c38fc67
Alex Walhout <alexpwalhout@gmail.com>
2018-11-10 14:08:43 -0500

    deleted old audio code

--- src/GameLogic.cpp
+++ src/GameLogic.cpp
@@ -13,13 +13,6 @@ GameLogic::GameLogic()
     loadedTextures = new TextureLoader();
     loadedAudio = new AudioLoader();
 
-
-    if (!level1Music.loadFromFile("assets/Level_1_Cut.ogg")) // Loads and initializes all sounds based on impact
-    std::cout << "Could not load Level 1 Music." << std::endl;
-
-    if (!level2Music.loadFromFile("assets/Level_2_Cut.ogg")) // Loads and initializes all sounds based on impact
-    std::cout << "Could not load Level 2 Music." << std::endl;
-
     backgroundMusic.setBuffer(loadedAudio->soundTrack[1]);
     backgroundMusic.setVolume(50);
     backgroundMusic.play();
