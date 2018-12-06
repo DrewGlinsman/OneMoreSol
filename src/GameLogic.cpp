d13efba9ff0de3754a43d81fa539857335342462
Drew Glinsman <31940539+DrewGlinsman@users.noreply.github.com>
2018-12-06 13:20:54 -0500

    applied darker night to the regular levels as well as boss levels

--- src/GameLogic.cpp
+++ src/GameLogic.cpp
@@ -1009,9 +1009,9 @@ void GameLogic::runLevel(sf::CircleShape& gameSky, MajorTom* majorTom, float tim
             gameSky.rotate(timePassed * levelSpeedModifier);
 
             if((gameSky.getRotation() > 90.f) && (gameSky.getRotation() < 130.f))
-                nightLayer.setFillColor(sf::Color(255,255,255,gameSky.getRotation()/3));
-            else if(gameSky.getRotation() > 130.f)
                 nightLayer.setFillColor(sf::Color(255,255,255,gameSky.getRotation()/2));
+            else if(gameSky.getRotation() > 130.f)
+                nightLayer.setFillColor(sf::Color(255,255,255,gameSky.getRotation()));
             else
                 nightLayer.setFillColor(sf::Color(255,255,255,0));
 
